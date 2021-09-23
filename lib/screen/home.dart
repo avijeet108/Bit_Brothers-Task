3706def24d7e86830f96b072bb922ffee8c82d40
avijeet108 <kumaravijeet108@gamil.com>
2021-09-24 00:10:31 +0530

    first commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,120 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_password_login/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:email_password_login/screen/login.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool spin = true;
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedinuser = UserModel();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      this.loggedinuser = UserModel.fromMap(value.data());
+      setState(() {
+        spin = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lime[200],
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: spin
+            ? Center(
+                child: CircularProgressIndicator(
+                color: Colors.teal,
+              ))
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 100,
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    'Hey! Welcome',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "${loggedinuser.username}",
+                    style: TextStyle(color: Colors.teal, fontSize: 20.0),
+                  ),
+                  Text(
+                    "${loggedinuser.email}",
+                    style: TextStyle(color: Colors.teal, fontSize: 20.0),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.teal,
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        logout(context);
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.logout,
+                            color: Colors.lime[200],
+                            size: 27.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'LogOut',
+                              style: TextStyle(
+                                  color: Colors.lime[200],
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Fluttertoast.showToast(msg: "Logged out successfully");
+
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => Login()));
+  }
+}
---
+++ lib/screen/login.dart
@@ -0,0 +1,226 @@
+import 'package:email_password_login/screen/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:email_password_login/screen/registration.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  //form key
+  final _formkey = GlobalKey<FormState>();
+
+  //editing controller
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  //firebase
+  final _auth = FirebaseAuth.instance;
+
+  bool _secure = false;
+  bool _passshow = true;
+
+  showhide() {
+    setState(() {
+      _secure = !_secure;
+      _passshow = !_passshow;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lime[200],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 100,
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Form(
+                    key: _formkey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: emailController,
+                          style: TextStyle(
+                            color: Colors.teal,
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return ("Please enter your email");
+                            }
+                            if (!RegExp(
+                                    "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                                .hasMatch(value)) {
+                              return ("Please enter a valid email");
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            emailController.text = value!;
+                          },
+                          cursorColor: Colors.teal,
+                          cursorHeight: 22.0,
+                          autofocus: false,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color: Colors.teal,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                                borderSide:
+                                    BorderSide(color: Colors.teal, width: 2.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                                borderSide: BorderSide(
+                                  color: Colors.teal,
+                                  width: 2.0,
+                                ),
+                              ),
+                              labelText: 'Email',
+                              labelStyle: TextStyle(color: Colors.teal)),
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        TextFormField(
+                          controller: passwordController,
+                          style: TextStyle(color: Colors.teal),
+                          cursorColor: Colors.teal,
+                          cursorHeight: 22.0,
+                          autofocus: false,
+                          obscureText: _passshow,
+                          validator: (value) {
+                            RegExp regex = new RegExp(r'^.{6,}$');
+                            if (value!.isEmpty) {
+                              return ("Password is required for login");
+                            }
+                            if (!regex.hasMatch(value)) {
+                              return ("Enter valid password(min 6 characters)");
+                            }
+                          },
+                          onSaved: (value) {
+                            passwordController.text = value!;
+                          },
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide:
+                                  BorderSide(color: Colors.teal, width: 2.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide(
+                                color: Colors.teal,
+                                width: 2.0,
+                              ),
+                            ),
+                            prefixIcon: Icon(
+                              Icons.vpn_key,
+                              color: Colors.teal,
+                            ),
+                            suffixIcon: IconButton(
+                              onPressed: showhide,
+                              icon: Icon(
+                                _secure
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Colors.teal,
+                              ),
+                            ),
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: Colors.teal),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        elevation: 5.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        backgroundColor: Colors.teal,
+                      ),
+                      onPressed: () {
+                        signin(emailController.text, passwordController.text);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Text(
+                          'LogIn',
+                          style: TextStyle(
+                              color: Colors.lime[200],
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(color: Colors.teal),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Reg()));
+                        },
+                        child: Text(
+                          ' SignUp',
+                          style: TextStyle(
+                              color: Colors.teal, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void signin(String email, String password) async {
+    if (_formkey.currentState!.validate()) {
+      await _auth
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((uid) => {
+                Fluttertoast.showToast(msg: "Login Successful"),
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => Home()))
+              })
+          .catchError((e) {
+        Fluttertoast.showToast(msg: e!.message);
+      });
+    }
+  }
+}
